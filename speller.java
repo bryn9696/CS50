e5a8271baa538857afd1417fca123e801f101465
bryn9696 <bryn2009@live.co.uk>
2022-11-28 15:55:30 +0000

    problem set 5, simple spell check

--- speller.java
+++ speller.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 public class speller {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter world to spellcheck: ");
+    System.out.print("Enter word to spellcheck: ");
     String input = scanner.nextLine();
     scanner.close();
 
     String word = input.toLowerCase();
-    System.out.println(word);
+
+    String[] dictionary = {"how", "toast", "beach", "computer", "nest"};
+
+    for (int i = 0; i < dictionary.length; i++) {
+      if (word.equals(dictionary[i])) {
+        System.out.println(input + " = " + dictionary[i]);
+      }
+    }
+
+    System.out.println("check complete");
   }
 }
