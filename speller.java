92cc8838ae91a798146bb30f7be0e4612c6c6e9a
bryn9696 <bryn2009@live.co.uk>
2022-11-29 15:44:59 +0000

    problem set 5, refactored

--- speller.java
+++ speller.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.swing.plaf.TreeUI;
-
 public class speller {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -13,7 +11,6 @@ public class speller {
     String input = scanner.nextLine();
     scanner.close();
 
-    int spellCount = 0;
     String word = input.toLowerCase();
     String[] words = word.split(" ");
     List<String> incorrectlySpeltWords = new ArrayList<>();
@@ -23,22 +20,21 @@ public class speller {
 
     for (int j = 0; j < words.length; j++) {
       for (int i = 0; i < dictionary.length; i++) {
-        if (!words[j].equals(dictionary[i])) {
-          if (!incorrectlySpeltWords.contains(words[j])) {
-            for (int k = 0; k < dictionary.length; k++) {
+        if (!words[j].equals(dictionary[i]) && !incorrectlySpeltWords.contains(words[j])) {
               if (Arrays.binarySearch(dictionary, words[j]) < 0) {
                 incorrectlySpeltWords.add(0, words[j]);
               }
               break;
-            }            
-          }
-          break;
         }
       }
     }
     
     Collections.reverse(incorrectlySpeltWords);
-    System.out.println("These words " + incorrectlySpeltWords + " are spelt incorrectly");
-    System.out.println("check complete");
+    if (incorrectlySpeltWords.size() > 1) {
+      System.out.println("These words " + incorrectlySpeltWords + " are spelt incorrectly.");
+    } else {
+      System.out.println("This word " + incorrectlySpeltWords + " is spelt incorrectly.");
+    }
+    System.out.println("Check complete!");
   }
 }
