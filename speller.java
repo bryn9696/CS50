4e83a4803292427df7ebadd088a1c55c3a2f5849
bryn9696 <bryn2009@live.co.uk>
2022-11-29 15:35:37 +0000

    prob set 5

--- speller.java
+++ speller.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.plaf.TreeUI;
 
 public class speller {
   public static void main(String[] args) {
@@ -9,20 +13,32 @@ public class speller {
     String input = scanner.nextLine();
     scanner.close();
 
+    int spellCount = 0;
     String word = input.toLowerCase();
     String[] words = word.split(" ");
+    List<String> incorrectlySpeltWords = new ArrayList<>();
 
-    System.out.println(Arrays.toString(words));
     String[] dictionary = {"how", "toast", "beach", "computer", "nest"};
+    Arrays.sort(dictionary);
 
-    for (int i = 0; i < dictionary.length; i++) {
-      for (int j = 0; j < words.length; j++) {
-        if (words[j].equals(dictionary[i])) {
-          System.out.println(words[j] + " = " + dictionary[i]);
+    for (int j = 0; j < words.length; j++) {
+      for (int i = 0; i < dictionary.length; i++) {
+        if (!words[j].equals(dictionary[i])) {
+          if (!incorrectlySpeltWords.contains(words[j])) {
+            for (int k = 0; k < dictionary.length; k++) {
+              if (Arrays.binarySearch(dictionary, words[j]) < 0) {
+                incorrectlySpeltWords.add(0, words[j]);
+              }
+              break;
+            }            
+          }
+          break;
         }
       }
     }
-
+    
+    Collections.reverse(incorrectlySpeltWords);
+    System.out.println("These words " + incorrectlySpeltWords + " are spelt incorrectly");
     System.out.println("check complete");
   }
 }
